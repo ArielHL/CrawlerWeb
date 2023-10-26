c81859de2212c6886d9851dd5d3effa99ad5b6e9
ArielHL <73503229+ArielHL@users.noreply.github.com>
2023-10-25 21:47:44 -0300

    Generate Translation

--- WC_6/src/MiddleWares/Translate_v3.py
+++ WC_6/src/MiddleWares/Translate_v3.py
@@ -1,48 +1,39 @@
 import pandas as pd
-from deep_translator import GoogleTranslator
+from googletrans import Translator
 from concurrent.futures import ThreadPoolExecutor
 from tqdm import tqdm
 import time
 import random
-from collections import OrderedDict
 
 class Translator:
+
+          
+    model = Translator()
     
-    # Defining Model
-    translator_model=GoogleTranslator(source='auto', target='en')
-    
-    def __init__(self, translate_model:object=translator_model):
-        Translator.translate_model = translate_model
-        
     @staticmethod
     def translate_text(sentence):
         
         # Split the sentence into chunks of 500 words each
         words = sentence.split()
-        chunks = [words[i:i+450] for i in range(0, len(words), 450)]
+        chunks = [words[i:i+490] for i in range(0, len(words), 490)]
         
         # Translate each chunk and join them back into a single sentence
         translated_chunks = []
         for chunk in chunks:
             chunk_sentence = ' '.join(chunk)
-            translated_chunk = Translator.translate_model.translate(chunk_sentence)
-            time.sleep(1.5)
-            
+            translated_chunk = Translator.model.translate(chunk_sentence, dest='en').text
             translated_chunks.append(translated_chunk)
+            time.sleep(random.random()*2)
         
         # Join the translated chunks into a single sentence
         translated_text = ' '.join(translated_chunks)
         return translated_text
 
-    @staticmethod
-    def translate_text_short(sentence):
-        if len(sentence) > 4500:
-            sentence = sentence[:4500]
-        translated_text = Translator.translate_model.translate(sentence)
-        return translated_text 
 
     @staticmethod
-    def translate_dataframe(df, column_name,num_workers:int=4):
+    def translate_dataframe(df:pd.DataFrame,
+                            column_name:str,
+                            num_workers:int=4):
         
         df['indexed_text']=df.index.astype(str)+'_'+df[column_name]
         
@@ -50,8 +41,7 @@ class Translator:
         rows_as_dicts = df.to_dict(orient='records')
 
         def translate_row(row):
-            translated_text = Translator.translate_text_short(row['indexed_text'])
-            time.sleep(2)
+            translated_text = Translator.translate_text(row['indexed_text'])
             return row['index'], translated_text
     
 
